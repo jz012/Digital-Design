9c110855c501e55009e057776a46bb671e0a5ef5
notjeffreyzhang <79546029+notjeffreyzhang@users.noreply.github.com>
2021-12-20 00:31:12 -0500

    Add files via upload

---
+++ Lab 7.srcs/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+// Date        : Tue Apr  6 03:32:42 2021
+// Host        : vm-12-Xilinx running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top dist_mem_gen_0 -prefix
+//               dist_mem_gen_0_ dist_mem_gen_0_stub.v
+// Design      : dist_mem_gen_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "dist_mem_gen_v8_0_13,Vivado 2020.1" *)
+module dist_mem_gen_0(a, spo)
+/* synthesis syn_black_box black_box_pad_pin="a[7:0],spo[15:0]" */;
+  input [7:0]a;
+  output [15:0]spo;
+endmodule
